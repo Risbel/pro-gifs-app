c2926474df7897bb2c57d3ebb019d5e2c52264ac
Risbel Suarez <risbel961019@gmail.com>
2023-04-22 18:40:09 -0400

    feat: Add new docs

---
+++ docs/static/js/139.9ffee839.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktraining=self.webpackChunktraining||[]).push([[139],{139:function(e,n,i){i.r(n);i(791);var t=i(184);n.default=function(){return(0,t.jsx)("div",{className:"px-8 py-16 text-slate-100",children:(0,t.jsx)("div",{children:"Details"})})}}}]);
+//# sourceMappingURL=139.9ffee839.chunk.js.map
---
+++ docs/static/js/983.a194fc15.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktraining=self.webpackChunktraining||[]).push([[983],{983:function(i,t,e){e.r(t);e(791);var a=e(184);t.default=function(){return(0,a.jsx)("div",{className:"px-4 md:px-12 py-32 text-slate-100 text-sm md:text-xl lg:text-2xl",children:(0,a.jsx)("div",{children:"This is an web application made with React and Tailwind css with the aim of easily searching for gift sets in the Giphy API provided by Meta."})})}}}]);
+//# sourceMappingURL=983.a194fc15.chunk.js.map
